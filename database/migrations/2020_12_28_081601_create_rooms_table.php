f26db8d8b0581cb22ba662a8c89442820b1e2cd9
Shahed Shanjid <mail.shahedshanjid@gmail.com>
2020-12-28 15:17:26 +0600

    migrations

---
+++ database/migrations/2020_12_28_081601_create_rooms_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateRoomsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('rooms', function (Blueprint $table) {
+            $table->id();
+            $table->string('launch_id');
+            $table->string('room_no');
+            $table->string('room_status')->default('ACTIVE');
+            $table->string('main_category');
+            $table->string('room_description')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('rooms');
+    }
+}
